84a97ab40428668a8e2fb8999bd658b3ca3429d9
YuliaNatasyaFDA <128021797+YuliaNatasyaFDA@users.noreply.github.com>
2023-03-21 11:01:03 +0000

    Update Biodata

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-  return "<p>Yulia Natasya</p>"
+  return "<p>Nama : Yulia Natasya Farah Diba Arifin</p><p>Tempat, tanggal lahir : Sampang, 09 Juli 2004</p><p>Alamat : Jln. Raya Krampon Barat</p><p>Jenis Kelamin : Perempuan</p><p>Umur : 19 tahun</p>"
 
 
 if __name__ == '__main__':
